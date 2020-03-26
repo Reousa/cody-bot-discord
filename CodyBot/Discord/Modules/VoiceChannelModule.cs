ffc75d5498315e8c712c5d8a985b70c66b2ab5ab
KareemSarhan <37156523+KareemSarhan@users.noreply.github.com>
2020-03-26 03:03:05 +0200

    voice channel module
    added come command(Join author's current voice channel.)

---
+++ CodyBot/Discord/Modules/VoiceChannelModule.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodyBot.Discord.Modules
+{
+    // Create a module with no prefix
+    public class VoiceChannelModule : ModuleBase<SocketCommandContext>
+    {
+        // ~come -> Joins your currnet room
+        [Command("come")]
+        [Summary("Join author's current voice channel.")]
+        public async Task ComeAsync( [Summary("Join author's current voice channel.")] IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null)
+            {
+                await Context.Channel.SendMessageAsync("User must be in a voice channel");
+            }
+
+            return;
+        }
+
+    }
+}
