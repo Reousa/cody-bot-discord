f989e1eb475fb8cf4a9db62cff958c7be1158b18
Karim <karim.essam.hamdy@gmail.com>
2020-03-26 02:13:12 +0200

    Echo module

---
+++ CodyBot/Discord/Modules/EchoModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodyBot.Discord.Modules
+{
+	// Create a module with no prefix
+	public class EchoModule : ModuleBase<SocketCommandContext>
+	{
+		// ~say hello world -> hello world
+		[Command("say")]
+		[Summary("Echoes a message back to the user.")]
+		public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+		{
+			return ReplyAsync(echo);
+		}
+	}
+}
