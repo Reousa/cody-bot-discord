b12728387f3f255acc99a124b13c782335a68f1e
KareemSarhan <37156523+KareemSarhan@users.noreply.github.com>
2020-03-26 06:02:14 +0200

    move command that moves the user
    refactoring
    todo tasks

--- CodyBot/Discord/Modules/VoiceChannelModule.cs
+++ CodyBot/Discord/Modules/VoiceChannelModule.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using RunMode = Discord.Commands.RunMode;
 
 namespace CodyBot.Discord.Modules
 {
@@ -23,6 +24,49 @@ namespace CodyBot.Discord.Modules
             {
                 await Context.Channel.SendMessageAsync("Author must be in a voice channel, or a voice channel must be passed as an argument.");
             }
+            return;
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------------------------------------------
+
+        // ~marmt -> Auther gets moved to all the empty voice channels and then gets moved back to his orignal channel
+        // ~marmt @kareemsarhan  -> metioned user gets moved to all the empty voice channels and then gets moved back to his orignal channel
+        [Command("move")]
+        [Alias("marmt", "bhdl")]
+        [Summary("moves the user around.")]
+        public async Task moveAsync([Summary("moves the user around.")] IGuildUser user = null , int count=10)
+        {
+            // Get the user
+            user = user as IGuildUser ?? Context.User as IGuildUser;
+            // Get the audio channel
+            IVoiceChannel orignalchannel = user.VoiceChannel;
+            if (orignalchannel == null)
+            {
+                await Context.Channel.SendMessageAsync("The user that you want to move must be in a voice channel");
+            }
+            IGuild guild = Context.Guild;
+            IReadOnlyCollection<IVoiceChannel> guildchannels = await guild.GetVoiceChannelsAsync();
+            //TODO: check this the problem in this link is happining here , cant get the solution to work 
+            //https://discord.foxbot.me/docs/faq/commands/general.html?tabs=cmdattrib
+            //its not an error it just slowes down the execution
+            for (int i = 0; count > i; i++)
+            {
+                foreach (IVoiceChannel channel in guildchannels)
+                {
+                    //TODO: mesh 3arf a al voice channel current members 3shan a check hya empty wla l2
+                    if (channel != orignalchannel)
+                        await user.ModifyAsync(x =>
+                        {
+                            x.Channel = Optional.Create(channel);
+                        });
+                }
+            }
+            //TODO: efhm how to convert from lamda exp to normal
+            await user.ModifyAsync(x =>
+            {
+                x.Channel = Optional.Create(orignalchannel);
+            });
             return;
         }
     }
