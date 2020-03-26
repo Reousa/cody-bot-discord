c4a5bfce2aaf08ba825548f2283892d9fdcd3d90
KareemSarhan <37156523+KareemSarhan@users.noreply.github.com>
2020-03-26 03:24:54 +0200

    Refactoring .

--- CodyBot/Discord/Modules/VoiceChannelModule.cs
+++ CodyBot/Discord/Modules/VoiceChannelModule.cs
@@ -10,20 +10,20 @@ namespace CodyBot.Discord.Modules
     // Create a module with no prefix
     public class VoiceChannelModule : ModuleBase<SocketCommandContext>
     {
-        // ~come -> Joins your currnet room
+        // ~come -> Joins your current room
+        // ~come room2  -> Joins room2
         [Command("come")]
+        [Alias("join", "t3ala")]
         [Summary("Join author's current voice channel.")]
-        public async Task ComeAsync( [Summary("Join author's current voice channel.")] IVoiceChannel channel = null)
+        public async Task ComeAsync( [Summary("Joins author's current voice channel or the specified channel .")] IVoiceChannel channel = null)
         {
             // Get the audio channel
             channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
             if (channel == null)
             {
-                await Context.Channel.SendMessageAsync("User must be in a voice channel");
+                await Context.Channel.SendMessageAsync("Author must be in a voice channel, or a voice channel must be passed as an argument.");
             }
-
             return;
         }
-
     }
 }
