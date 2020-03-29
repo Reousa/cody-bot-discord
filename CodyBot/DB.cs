d047c8be88c3db0f60d0b5cec3962b55bc82d2e3
KareemSarhan <37156523+KareemSarhan@users.noreply.github.com>
2020-03-29 09:48:45 +0200

    DB Json file
    this file has the id and the prefix of any server that has a custom prefix .

---
+++ CodyBot/DB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodyBot
+{
+    public class DB
+    {
+        public ulong GuildId
+        {
+            get;
+            set;
+        }
+        public char Prefix
+        {
+            get;
+            set;
+        }
+    }
+}
