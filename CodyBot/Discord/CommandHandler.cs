0d6754e111840c2d6ff9105e1f827e4c71219932
KareemSarhan <37156523+KareemSarhan@users.noreply.github.com>
2020-03-29 09:49:23 +0200

    added prefix checker

--- CodyBot/Discord/CommandHandler.cs
+++ CodyBot/Discord/CommandHandler.cs
@@ -1,7 +1,10 @@
 ﻿using Discord.Commands;
 using Discord.WebSocket;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +17,43 @@ namespace CodyBot.Discord
 		private readonly DiscordSocketClient client;
 		private readonly CommandService commands;
 
+		public static List<DB> Reader() 
+		{
+			string RawJson = File.ReadAllText(@"C:/Users/karee/source/repos/cody-bot-discord/CodyBot/DB.Json");
+			return JsonConvert.DeserializeObject<List<DB>>(RawJson);
+		}
+		public static void Writer(ulong Id,char Prefix) 
+		{
+
+			List<DB> _data = Reader();
+			for (int i = 0; i < _data.Count; i++)
+				if (_data[i].GuildId == Id) {
+					_data.RemoveAt(i);
+					break;
+				}
+			_data.Add(new DB()
+			{
+				GuildId=Id,
+				Prefix=Prefix
+			});
+			string json = JsonConvert.SerializeObject(_data.ToArray());
+			//write string to file
+			System.IO.File.WriteAllText(@"C:/Users/karee/source/repos/cody-bot-discord/CodyBot/DB.Json", json);
+		}
 		public CommandHandler(DiscordSocketClient client, CommandService commands)
 		{
 			this.commands = commands;
 			this.client = client;
 		}
-
+		public char PrefixGetter(ulong GuildID)
+		{
+			List<DB> allista = Reader();
+			foreach(DB server in allista) { 
+				if(server.GuildId==GuildID)
+					return server.Prefix;
+			}
+			return prefix;
+		}
 		public async Task InstallCommandsAsync()
 		{
 			// Hook the MessageReceived event into our command handler
@@ -40,15 +74,14 @@ namespace CodyBot.Discord
 			// Create a number to track where the prefix ends and the command begins
 			int argPos = 0;
 
+			Console.WriteLine("asdsadsadsadsadsadsadsadsadsad");
+			// Create a WebSocket-based command context based on the message
+			var context = new SocketCommandContext(client, message);
 			// Determine if the message is a command based on the prefix and make sure no bots trigger commands
-			if (!(message.HasCharPrefix(prefix, ref argPos) ||
+			if (!(message.HasCharPrefix(PrefixGetter(context.Guild.Id), ref argPos) ||
 				message.HasMentionPrefix(client.CurrentUser, ref argPos)) ||
 				message.Author.IsBot)
 				return;
-
-			// Create a WebSocket-based command context based on the message
-			var context = new SocketCommandContext(client, message);
-
 			// Execute the command with the command context we just
 			// created, along with the service provider for precondition checks.
 
